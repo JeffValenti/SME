e2e35555c7d33ed97f52ec942821986f1645589d
Jeff Valenti <valenti@stsci.edu>
2018-05-12 00:21:26 -0400

    include_package_data=True

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from distutils.core import setup
 
 setup(
-        name = 'sme',
-        version = '0.0',
-        description = 'Spectroscopy Made Easy',
-        author = 'Jeff A. Valenti',
-        author_email = 'valenti@stsci.edu',
-        package_dir = {'': 'src'},
-        packages = ['sme']
+        name='sme',
+        version='0.0',
+        description='Spectroscopy Made Easy',
+        author='Jeff A. Valenti',
+        author_email='valenti@stsci.edu',
+        package_dir={'': 'src'},
+        packages=['sme']
+        include_package_data=True
         )
