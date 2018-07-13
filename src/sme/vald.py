0a076528a6c7f1e45c8ec034e367d8aa4f793f8b
Jeff Valenti <valenti@stsci.edu>
2018-07-13 15:52:30 -0400

    add ValdFile class

--- src/sme/vald.py
+++ src/sme/vald.py
@@ -1,17 +1,22 @@
+class FileError(Exception):
+    """Raise when attempt to read a VALD line data file fails.
+    """
+
+
 class Line:
     """Atomic data for a single spectral line.
     """
     def __init__(self, species, wlcent, excit, gflog, gamrad, gamqst, gamvw):
-        self.species = species
-        self.wlcent = wlcent
-        self.excit = excit
-        self.gflog = gflog
-        self.gamrad = gamrad
-        self.gamqst = gamqst
-        self.gamvw = gamvw
+        self.species = str(species)
+        self.wlcent = float(wlcent)
+        self.excit = float(excit)
+        self.gflog = float(gflog)
+        self.gamrad = float(gamrad)
+        self.gamqst = float(gamqst)
+        self.gamvw = float(gamvw)
 
     def __str__(self):
-        return "'{}',{:10.4f},{:7.4f},{:7.3f},{:5.2f},{:6.2f},{:8.3f}". \
+        return "'{}',{:10.4f},{:7.4f},{:7.3f},{:6.3f},{:7.3f},{:8.3f}". \
             format(
                 self.species,
                 self.wlcent,
@@ -75,10 +80,59 @@ class ValdFile:
     """Atomic data for a list of spectral lines.
     """
     def __init__(self, filename):
+        self._filename = filename
         self.read(filename)
 
-    def read(filename):
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def linelist(self):
+        return self._linelist
+
+    @property
+    def valdatmo(self):
+        return self._valdatmo
+
+    def read(self, filename):
         """Read line data file from the VALD extract stellar service.
         """
         with open(filename, 'r') as file:
             lines = file.readlines()
+        self.parse_header(lines[0])
+        self._linelist = self.parse_linedata(lines[3:3+self._nlines])
+        self._valdatmo = self.parse_valdatmo(lines[3+self._nlines])
+
+    def parse_header(self, line):
+        """Parse header line from a VALD line data file.
+        """
+        words = [w.strip() for w in line.split(',')]
+        if len(words) < 5 or words[5] != 'Wavelength region':
+            raise FileError(f'{self._filename} is not a VALD line data file')
+        self._wavelo = float(words[0])
+        self._wavehi = float(words[1])
+        self._nlines = int(words[2])
+        self._nlines_proc = int(words[3])
+        self._vmicro = float(words[4])
+
+    def parse_linedata(self, lines):
+        """Parse line data from a VALD line data file.
+        """
+        linelist = LineList()
+        for line in lines:
+            words = [w.strip() for w in line.split(',')]
+            assert len(words) >= 8
+            spec_ion = words[0]
+            if spec_ion[0] == "'" and spec_ion[-1] == "'":
+                spec_ion = spec_ion[1:-1]
+            linelist.add(spec_ion, *words[1:3], *words[4:8])
+        return linelist
+
+    def parse_valdatmo(self, line):
+        """Read VALD model atmosphere line from a VALD line data file.
+        """
+        lstr = line.strip()
+        if lstr[0] != "'" or lstr[-2:] != "',":
+            raise FileError(f'error parsing model atmosphere: {lstr}')
+        return lstr[1:-2]
